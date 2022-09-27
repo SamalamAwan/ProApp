c48586ba74068c8065d937111b322a8c4a73c579
Sam Awan <sam.awan@ewistore.co.uk>
2022-09-27 13:33:09 +0100

    second commit

--- screens/Home.js
+++ screens/Home.js
@@ -7,7 +7,7 @@ export const HomeScreen = ({ navigation }) => {
   return (
     <ScreenContainer>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch' }}>
-      <Text>HOME WELCOME!</Text>
+      <Text>HOME WELCOME2!</Text>
       <Card mode={"contained"} onPress={() => navigation.navigate('Forms')}>
     <Card.Title title="Go To Forms" />
   </Card>
